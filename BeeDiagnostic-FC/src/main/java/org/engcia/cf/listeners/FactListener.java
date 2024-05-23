22a79a895deb048790c76bba99b2213fcc79bb81
1200884 <1200884@isep.ipp.pt>
2024-05-23 16:56:06 +0100

    vski

--- BeeDiagnostic-FC/src/main/java/org/engcia/cf/listeners/FactListener.java
+++ BeeDiagnostic-FC/src/main/java/org/engcia/cf/listeners/FactListener.java
@@ -1,7 +1,5 @@
 package org.engcia.cf.listeners;
 
-import org.engcia.cf.model.Evidence;
-import org.engcia.cf.model.Hypothesis;
 import org.kie.api.event.rule.ObjectDeletedEvent;
 import org.kie.api.event.rule.ObjectInsertedEvent;
 import org.kie.api.event.rule.ObjectUpdatedEvent;
@@ -33,18 +31,11 @@ public class FactListener implements RuleRuntimeEventListener{
 	@Override
 	
 public void objectUpdated(ObjectUpdatedEvent event) {
-    Object obj = event.getObject();
-    if (obj instanceof Evidence) {
-        Evidence evidence = (Evidence) obj;
-        double newCF = evidence.getCf(); // get the new CF value
-        System.out.println("Updated CF value for Evidence: " + newCF);
-        
-        // update the CF value for the Hypothesis object
-        Hypothesis hypothesis = (Hypothesis) TrackingAgendaListener.getFactRef(Hypothesis.class, "varroa", "true");
-        hypothesis.updateConfidence(newCF);
-		System.out.println("Updated CF value for Hypothesis: "+hypothesis.getCf());
+	System.out.println(
+				"Updated: " +
+				event.getObject().toString()
+				);
 		
     }
-}
 
 }

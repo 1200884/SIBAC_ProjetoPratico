1e0a90863d86d7310863480695309c1b906537e1
1200884 <1200884@isep.ipp.pt>
2024-05-28 16:01:55 +0100

    vski

--- Bee-FC/src/main/java/org/engcia/cfsample/DroolsTest.java
+++ Bee-FC/src/main/java/org/engcia/cfsample/DroolsTest.java
@@ -1,6 +1,8 @@
 package org.engcia.cfsample;
 
 //import org.drools.core.rule.builder.dialect.asm.ClassGenerator;
+import java.util.Scanner;
+
 import org.engcia.cf.listeners.FactListener;
 import org.engcia.cf.listeners.TrackingAgendaListener;
 import org.engcia.cf.model.Evidence;
@@ -21,7 +23,8 @@ public class DroolsTest {
 	        KieServices ks = KieServices.Factory.get();
     	    KieContainer kContainer = ks.getKieClasspathContainer();
         	KieSession kSession = kContainer.newKieSession("ksession-rules");
-        	
+            Scanner scanner = new Scanner(System.in);
+
         	// Agenda listener
         	kSession.addEventListener(new TrackingAgendaListener());
         	
@@ -41,18 +44,36 @@ public class DroolsTest {
             kSession.insert(new Hypothesis(0.0,"ascosferiose", "true"));
             kSession.insert(new Hypothesis(0.0,"virose", "true"));
             kSession.insert(new Hypothesis(0.0,"virusdaparalisiaaguda", "true"));
-            int CfObsMortalidadeAumentada,CfObsDeformidadesNasAsas,CfObsDiminuicaoDeProdutividade,CfObsAcarosVermelhosNasAbelhas,CfObsAbdomenInchado,CfObsIncapacidadeDeVoar,CfObsManchasEscurasNaColmeia,CfObsLarvasMortasOuApodrecidas,CfObsOperculoDeprimidoOuPerfurado,CfObsCriacaoMalSemeada,CfObsMauCheiro,CfObsTesteDoPalito,CfObsOperculoAfundado,CfObsLarvasMumificadas,CfObsOperculosEsburacados,CfObsCriacaoEmMosaico,CfObsAusenciaDeCheiro,CfObsLarvasMortasNumSacoComLiquido,CfObsMudançaDeCorLarvas,CfObsEscamaCastanhaAposAMorteDaLarvaEmFormaDeGondola,CfObsFacilidadeDeRemoçaoDaEscamaDaLarvaComUmPalito,CfObsNecroseEEnegrecimentoDasTraqueiasDasAbelhas,CfObsAbelhasArrastaremSe,CfObsAbelhasMortasNaEntradaDaColmeia,CfObsAbelhasMortasEmCaudaDeCometa,CfObsAblehasMortasEmDiferentesEstadosDeDesenvolvimento,CfObsAbelhasMortasComACabeçaDentroDasCelulas,CfObsDiminuicaoDoTamanhoDasAbelhas,CfObsFaltaDeReservas,CfObsPerdaDePelo,CfObsMovimentosTremidosDoCorpoEAsasDasAbelhasInfetadas,CfObsAspetoLuzidioEBrilhante;
-            final int CfObsVerao,CfObsInverno,CfObsPrimavera,CfObsOutono=1;
+            int CfObsMortalidadeAumentada,CfObsDeformidadesNasAsas,CfObsDiminuicaoDeProdutividade,CfObsAcarosVermelhosNasAbelhas,CfObsAbdomenInchado,CfObsIncapacidadeDeVoar,CfObsManchasEscurasNaColmeia,CfObsLarvasMortasOuApodrecidas,CfObsOperculoDeprimidoOuPerfurado,CfObsCriacaoMalSemeada,CfObsMauCheiro,CfObsTesteDoPalito,CfObsOperculoAfundado,CfObsLarvasMumificadas,CfObsOperculosEsburacados,CfObsCriacaoEmMosaico,CfObsAusenciaDeCheiro,CfObsLarvasMortasNumSacoComLiquido,CfObsMudançaDeCorLarvas,CfObsEscamaCastanhaAposAMorteDaLarvaEmFormaDeGondola,CfObsNecroseEEnegrecimentoDasTraqueiasDasAbelhas,CfObsAbelhasArrastaremSe,CfObsAbelhasMortasNaEntradaDaColmeia,CfObsAbelhasMortasEmCaudaDeCometa,CfObsAblehasMortasEmDiferentesEstadosDeDesenvolvimento,CfObsAbelhasMortasComACabeçaDentroDasCelulas,CfObsDiminuicaoDoTamanhoDasAbelhas,CfObsFaltaDeReservas,CfObsPerdaDePelo,CfObsMovimentosTremidosDoCorpoEAsasDasAbelhasInfetadas,CfObsAspetoLuzidioEBrilhante;
+            final int CfObsVerao=1,CfObsInverno=1,CfObsPrimavera=1,CfObsOutono=1;
             String estacao;
             System.out.println("Olá, caro utilizador! Por favor responda às próximas perguntas com os detalhes do que observou");
             System.out.println("-----------------------------------------------");
             System.out.println("Em que estação do ano se encontra?");
+            estacao = scanner.nextLine();
+            if(estacao.equals("Verão")){
+                kSession.insert(new Evidence( CfObsVerao,"Verao", "true"));
+
+            }
+            if(estacao.equals("Inverno")){
+                kSession.insert(new Evidence( CfObsInverno,"Inverno", "true"));
+
+                
+            }
+            if(estacao.equals("Outono")){
+                kSession.insert(new Evidence( CfObsOutono,"Outono", "true"));
+
+                
+            }
+            if(estacao.equals("Primavera")){
+                kSession.insert(new Evidence( CfObsPrimavera,"Primavera", "true"));
+
             
-            if(estacao)
-            kSession.insert(new Evidence( 0.5,"Mortalidade aumentada", "true"));
+            }
+           /*  kSession.insert(new Evidence( 0.5,"Mortalidade aumentada", "true"));
             kSession.insert(new Evidence( 0.5,"Deformidades nas asas", "true"));
             kSession.insert(new Evidence(0.5,"Diminuicao de produtividade", "true"));
-            kSession.insert(new Evidence( 0.5,"Acaros vermelhos nas abelhas", "true"));
+            kSession.insert(new Evidence( 0.5,"Acaros vermelhos nas abelhas", "true"));*/
             kSession.fireAllRules();
             
         } catch (Throwable t) {
